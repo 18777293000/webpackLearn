8b0e4d712964e8f63ca7b78a738bb4df17c1bcbb
18777293000 <120953990@qq.com>
2020-08-25 22:34:02 +0800

    关闭对dist文件的上传,webpack.prod.js运行成功，问题是因为代码写错了

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import {cube} from './math.js';
+import { cube } from './math.js';
 
-if(process.env.NODE_ENV !== 'production'){
-	console.log('look now node is' + process.env.NODE_ENV);
-}
+ if (process.env.NODE_ENV !== 'production') {
+   console.log('Looks like we are in development mode!'+ process.env.NODE_ENV);
+ }
 
-function component() {
+  function component() {
     var element = document.createElement('pre');
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = [
-+     'Hello webpack!',
-+     '5 cubed is equal to ' + cube(5)
-+   ].join('\n\n');
+      'Hello webpack!',
+      '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
-}
+  }
 
-document.body.appendChild(component());
+  document.body.appendChild(component());
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,16 @@
-const webpack = require('webpack');
-const {merge} = require('webpack-merge');
-const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+  const webpack = require('webpack');
+  const {merge} = require('webpack-merge');
+  const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+  const common = require('./webpack.common.js');
 
-module.exports = merge(common,{
-	devtool: "source-map",
-	plugin: [
-		new UglifyJSPlugin({
-			sourceMap: true
-		}),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('production')
-		})
-	]
-});
+  module.exports = merge(common, {
+    devtool: 'source-map',
+    plugins: [
+      new UglifyJSPlugin({
+        sourceMap: true
+     }),
+     new webpack.DefinePlugin({
+       'process.env.NODE_ENV': JSON.stringify('production')
+     })
+    ]
+  });
